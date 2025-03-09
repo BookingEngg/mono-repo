40fc34f1236ea334a66ea87137bcf1548439ccd9
Tushar Chand Thakur <74068671+tusharthakurepc1@users.noreply.github.com>
2025-03-10 00:10:00 +0530

    Niginx Reverse Proxy (#8)
    * feat: docker file for BE
    
    * feat: Fetch BE config from secrets for docker container
    
    * feat: Provide PAT key at docker-compose run time
    
    * fix: executing fetch-config script & feat: passing pat during args at runtime
    
    * feat: nignx -- reverse proxy --
    
    * add: nginx.conf to src file

---
+++ backend/fetch-config.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e  # Exit immediately if a command exits with a non-zero status
+
+# Ensure the GitHub URL is provided
+if [ -z "$GITHUB_PAT" ]; then
+  echo "Error: GITHUB_PAT is not set."
+  exit 1
+fi
+
+SECRETS_GITHUB_REPO="https://${GITHUB_PAT}@github.com/tusharthakurepc1/secrets"
+
+# Clone the repo, copy the config, and clean up
+git clone "$SECRETS_GITHUB_REPO" /tmp/repo --depth=1
+
+# Copy the prod config
+cp /tmp/repo/mono-repo/config.prod.json /app/src/config/config.prod.json
+# Copy the nginx config to shared folder
+# mkdir -p /app/shared
+# cp /tmp/repo/mono-repo/nginx.conf /app/shared/nginx.conf
+
+# Remove the secret cloned repo
+rm -rf /tmp/repo
+
+echo "Config successfully placed at /dist/src/config/config.prod.json"
