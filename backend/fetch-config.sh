b94531afc7264637f697f4511d0c83f63763250f
tushar-blitz <tusharthakurepc205@gmail.com>
2025-03-09 18:16:51 +0530

    feat: nignx -- reverse proxy --

--- backend/fetch-config.sh
+++ backend/fetch-config.sh
@@ -11,8 +11,14 @@ SECRETS_GITHUB_REPO="https://${GITHUB_PAT}@github.com/tusharthakurepc1/secrets"
 
 # Clone the repo, copy the config, and clean up
 git clone "$SECRETS_GITHUB_REPO" /tmp/repo --depth=1
-mkdir -p /dist/src/config
+
+# Copy the prod config
 cp /tmp/repo/mono-repo/config.prod.json /app/src/config/config.prod.json
+# Copy the nginx config to shared folder
+mkdir /app/shared
+cp /tmp/repo/mono-repo/nginx.conf /app/shared/nginx.conf
+
+# Remove the secret cloned repo
 rm -rf /tmp/repo
 
 echo "Config successfully placed at /dist/src/config/config.prod.json"
