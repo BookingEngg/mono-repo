d56053554962e01125d78b504145a4a913385798
tushar-blitz <tusharthakurepc205@gmail.com>
2025-03-14 03:32:40 +0530

    feat: google oauth signup and login

--- backend/src/services/user.service.ts
+++ backend/src/services/user.service.ts
@@ -1,10 +1,30 @@
 import UserDao from "@/dao/user.dao";
 import { IUser } from "@/interfaces/user.interface";
+import { TokenPayload } from "google-auth-library";
 
 class UserService {
   private userDao = new UserDao();
 
-  public createUser = async () => {};
+  public createUser = async (payload: TokenPayload) => {
+    const formattedUser = {
+      first_name: payload.given_name,
+      last_name: payload.family_name,
+      email: payload.email,
+      email_verified: payload.email_verified,
+      user_profile_picture: payload.picture,
+      
+      roles: [],
+      level: 1,
+      friends_ids: [],
+      requested_friends: [],
+      blocked_user: [],
+    };
+    return await this.userDao.createUser(formattedUser);
+  };
+
+  public getInhouseUserDetailsByEmail = async (email) => {
+    return this.userDao.getUserByEmail(email);
+  };
 
   public getChatUsers = async (user: IUser) => {
     const chatUsers = await this.userDao.getUserByUserIds(user.friends_ids);
