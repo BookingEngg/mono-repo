e13c9252f29b0f1490d0516bef4ed20392775679
tushar-blitz <tusharthakurepc205@gmail.com>
2025-04-20 20:12:46 +0530

    fix: body base layout scroll style

--- frontend/src/layout/Body/Body.tsx
+++ frontend/src/layout/Body/Body.tsx
@@ -1,11 +1,22 @@
 import { PropsWithChildren } from "react";
 import style from "./Body.module.scss";
 import classNames from "classnames/bind";
+import { isUserAuthorized } from "@/store/auth";
+import { useAppSelector } from "@/store/hooks";
 
 const cx = classNames.bind(style);
 
 const Body = ({ children }: PropsWithChildren): JSX.Element => {
-  return <section className={cx("content")}>{children}</section>;
+  const isAuthorized = useAppSelector(isUserAuthorized);
+  return (
+    <section
+      className={cx(
+        `${isAuthorized ? "content-auth" : "content-login"}`
+      )}
+    >
+      {children}
+    </section>
+  );
 };
 
 export default Body;
