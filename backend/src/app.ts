69918ca085c66aeb91d0f39ca9c518cb3c118966
tushar-blitz <tusharthakurepc205@gmail.com>
2025-11-11 18:24:46 +0530

    exep: redis adapter

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,19 @@
+// Modules
 import http from "http";
 import express from "express";
 import cors from "cors";
 import cookieSession from "cookie-session";
 import cookieParser from "cookie-parser";
-import morgan from "morgan";
+// Redis Module
+import { createClient } from "redis";
+// Socket Modules
 import { Server } from "socket.io";
+import { createAdapter } from "@socket.io/redis-adapter";
+// Socket Events
 import SocketEvents from "@/events/socket.events";
+// Logger Module
+import morgan from "morgan";
+// Configs
 import {
   PORT,
   serviceName,
@@ -14,6 +22,9 @@ import {
   SOCKET_EVENTS_NAMES,
   tokenDetails,
 } from "@config";
+// Utils
+import { getRedisUrl } from "./util/utils.util";
+// Types
 import { Routes } from "@interfaces/common.interface";
 
 class App {
@@ -85,7 +96,15 @@ class App {
     );
   }
 
-  public initilizeSocketEvents = () => {
+  public initilizeSocketEvents = async () => {
+    const redisPubClient = createClient({ url: getRedisUrl() });
+    const redisSubClient = redisPubClient.duplicate();
+
+    // Connect the redis publisher and subscriber
+    await Promise.all([redisPubClient.connect(), redisSubClient.connect()]);
+
+    this.io.adapter(createAdapter(redisPubClient, redisSubClient));
+
     this.io.on("connection", (socket) => {
       SOCKET_EVENTS_NAMES.forEach((eventName) => {
         socket.on(
